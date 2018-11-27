cabbb65394d997e2afac5aeebc4db2721e0559c5
it8303 <it8303@gmail.com>
2018-11-27 21:27:14 +0800

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//This section list the required libraries
+var express = require("express");
+//Create an instance of express server
+var app = express(); 
+
+app.get("/",function(request,respond){
+	respond.send('<h1>This is my web app');
+});
+
+//Start the express server tp listen to a port in the server
+//cannot use Promise here.....
+var listener = app.listen(
+    process.env.PORT,
+    process.env.IP,
+    function(){
+	console.log("server has started");
+	console.log('Listening on port ' + listener.address().port);
+});
